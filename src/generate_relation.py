42c508f612ba1e675b48f307a428b52463fee53c
zhangke1 <zhangke1@genomics.cn>
2022-04-13 15:32:08 +0800

    read file

--- src/generate_relation.py
+++ src/generate_relation.py
@@ -2,23 +2,31 @@ import pandas as pd
 
 
 def handle_gwas_association():
-    pass
+    df_gwas = pd.read_csv(
+        "data/gwas/gwas_catalog_v1.0-associations_e105_r2021-12-21.tsv",
+        sep="\t", dtype=str
+    ).fillna("")
+
+    print()
 
 
 def handle_omim_map():
-    pass
+    df_omim_map = pd.read_csv("data/OMIM/genemap2.txt", sep="\t", dtype=str, skiprows=3).fillna("")
 
-
-def handle_chpo():
-    pass
+    print()
 
 
 def handle_omim_chpo_rel():
-    pass
+    df_rel = pd.read_csv("data/omim_chn/omim_gene.txt", sep="\t", dtype=str).fillna("")
+
+    df_omim = pd.read_csv("data/omim_chn/OMIM_disease.txt", sep="\t", dtype=str).fillna("")
+
+    df_chpo = pd.read_csv("data/omim_chn/CHPO.txt", sep="\t", dtype=str).fillna("")
+
+    print()
 
 
 if __name__ == "__main__":
     handle_gwas_association()
     handle_omim_map()
-    handle_chpo()
     handle_omim_chpo_rel()
