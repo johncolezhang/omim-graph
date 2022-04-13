b85b33a53b8cecfe1c2652e5e9ad26436c4a3c4e
zhangke1 <zhangke1@genomics.cn>
2022-04-13 14:23:30 +0800

    initial project

---
+++ src/generate_relation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def handle_gwas_association():
+    pass
+
+
+def handle_omim_map():
+    pass
+
+
+def handle_chpo():
+    pass
+
+
+def handle_omim_chpo_rel():
+    pass
+
+
+if __name__ == "__main__":
+    handle_gwas_association()
+    handle_omim_map()
+    handle_chpo()
+    handle_omim_chpo_rel()
